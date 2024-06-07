af9b04f5d61ff134720c46551be79f5e6399617d
Team-32-Skilvull <team32skilvul@gmail.com>
2024-06-07 22:05:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
-import joblib 
+import joblib
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, WebRtcMode, RTCConfiguration
 import logging
 
@@ -147,9 +147,7 @@ def app():
             video_processor_factory=VideoTransformer,
             media_stream_constraints={"video": True, "audio": False},
             async_processing=True,
-            rtc_configuration=RTCConfiguration(
-                {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-            ),
+            rtc_configuration=RTC_CONFIGURATION,
         )
     elif mode == 'Unggah Gambar':
         uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "png", "jpeg"])
